342c75785d02208fa64382c26237a80751ec865b
riken95 <ebraud95@gmail.com>
2023-10-27 09:54:17 +0200

    Création train.c

---
+++ train.c
@@ -0,0 +1,18 @@
+
+
+
+
+/**
+ * @brief  Fonction principale
+ * @note    D'abord lit les arguments
+ *          Ensuite initialise la connexion au serveur
+ *          Ensuite avec un thread maintient la connexion avec le serveur
+ *          Ensuite démarre la logique pour avancer
+ *              Si la connexion au serveur s'arrête on relance une tentative de connexion
+ *              ...
+ * @param  argc: longueur des arguments
+ * @param  argv[]: port du serveur - nom du train
+ * @retval 
+ *
+*/
+int main(int argc, char * argv[]);
