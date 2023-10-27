c96274684bc366be1a819867d4bd2b038a0f4e57
riken95 <ebraud95@gmail.com>
2023-10-27 10:29:39 +0200

    Importation des librairies  et des constantes symboliques

--- server.c
+++ server.c
@@ -1,4 +1,31 @@
 
+
+
+//Importation des librairies
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h> /* close , pipe...*/
+#include <netdb.h> /* gethostbyname */
+#include <pthread.h>
+#include <string.h>
+
+
+//Constantes utiles pour le code
+#define INVALID_SOCKET -1
+#define SOCKET_ERROR -1
+#define closesocket(s) close(s)
+typedef int SOCKET;
+typedef struct sockaddr_in SOCKADDR_IN;
+typedef struct sockaddr SOCKADDR;
+typedef struct in_addr IN_ADDR;
+#define MAX_BUFFER 256
+
+
+
+
+
 /**
  * @brief  Définit une structure qui contient toutes les variables utiles
  *         pour gérer un train.
