f9890069a86772251508bfe8519b2ce1adca0e63
riken95 <ebraud95@gmail.com>
2023-11-01 17:59:04 +0100

    Ajout du mécanisme de connexions entrantes

--- server.c
+++ server.c
@@ -4,7 +4,7 @@
 void initialiser_socket_connexions_tcp_entrantes(SOCKET * sock, SOCKADDR_IN * csin){
 
     //Initialisation de l'adresse d'écoute
-    socklen_t sinsize = sizeof csin;
+    socklen_t sinsize = sizeof *csin;
     csin->sin_family = AF_INET;
     csin->sin_addr.s_addr = INADDR_ANY;
     csin->sin_port = htons(port);
@@ -36,26 +36,64 @@ void lecture_arguments(int argc, char * argv[]){
     }
 }
 
+void * accepter_connexions_tcp(void * arg){
+  
+    struct ThreadData * Donnees_Thread = (struct ThreadData *) arg;
+    struct ArgConnexionsEntrantes * SocketsEtAdresse = Donnees_Thread->shared_data;
+
+    SOCKET SocketNouvelleConnexion_Temporaire;
+    socklen_t sinsize = sizeof *(SocketsEtAdresse->csin);
+    
+    while(1){
+      SocketNouvelleConnexion_Temporaire = accept(SocketsEtAdresse->sock, (SOCKADDR *) SocketsEtAdresse->csin,&sinsize);
+      if(SocketNouvelleConnexion_Temporaire == INVALID_SOCKET){
+        printf("Erreur socket entrante\n");
+      }else{
+        printf("Nouvelle connexion entrante !\n");
+      }
+      pthread_mutex_lock(&Donnees_Thread->mutex); //On bloque la mémoire aux autres threads avant écriture
+      *(SocketsEtAdresse->sock_cible) = SocketNouvelleConnexion_Temporaire;
+      printf("Socket : %d\n",SocketNouvelleConnexion_Temporaire);
+      pthread_mutex_unlock(&Donnees_Thread->mutex);
+      
+    }
+    
+}
+
 int main(int argc, char * argv[]){
 
     //Assignation du port
     
     lecture_arguments(argc,argv);
 
+
     //Initialisation de la lecture des entrées utilisateur
 
     char * EntreesUtilisateur = (char *) malloc(sizeof(char)*MAX_BUFFER);
     ThreadData dataThreadEntreesUtilisateur;
     dataThreadEntreesUtilisateur.shared_data = EntreesUtilisateur;
-    pthread_create(&(dataThreadEntreesUtilisateur.thread),NULL,entrees_utilisateur,(void *) dataThreadEntreesUtilisateur.shared_data);
+    pthread_create(&(dataThreadEntreesUtilisateur.thread),NULL,entrees_utilisateur,
+    (void *) &dataThreadEntreesUtilisateur);
 
 
+    //Initialisation du socket d'écoue et de l'adresse d'écoute
     struct ArgConnexionsEntrantes socket_source_cible;
     initialiser_socket_connexions_tcp_entrantes(socket_source_cible.sock,socket_source_cible.csin);
 
-    SOCKET * SocketNouvellesConnexions = (SOCKET *) malloc(sizeof(SOCKET));
+
+    //Socket qui va recevoir les nouvelles connexions
+    SOCKET * SocketNouvellesConnexions;
+    socket_source_cible.sock_cible = SocketNouvellesConnexions;
+
+
+    //Lancement du processus d'écoute permanente de nouvelles connexions
     ThreadData dataAccepterConnexions;
-    dataAccepterConnexions.shared_data = SocketNouvellesConnexions;
+    dataAccepterConnexions.shared_data = &socket_source_cible;
     
+    pthread_create(&(dataAccepterConnexions.thread),NULL,accepter_connexions_tcp,
+    (void *) &dataAccepterConnexions);
+    
+
+
 
 }
